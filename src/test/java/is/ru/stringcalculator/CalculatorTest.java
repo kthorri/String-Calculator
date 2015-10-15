e6cb1ee1399cb29374ff661b40bc2bb77e3fc0c5
Thorri <kristinn.thorri@gmail.com>
2015-10-15 13:55:27 +0000

    added tests for combined delimiters

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -6,55 +6,54 @@ import org.junit.Test;
 public class CalculatorTest {
 
 	public static void main(String args[]) {
-      org.junit.runner.JUnitCore.main("is.ru.stringcalculator.CalculatorTest");
-    }
-
+  	org.junit.runner.JUnitCore.main("is.ru.stringcalculator.CalculatorTest");
+	}
 	@Test
 	public void testEmptyString() {
 		assertEquals(0, Calculator.add(""));
 	}
-
 	@Test
 	public void testOneNumber() {
 		assertEquals(1, Calculator.add("1"));
 	}
-
 	@Test
 	public void testTwoNumbers() {
 		assertEquals(3, Calculator.add("1,2"));
 	}	
-
 	@Test
-    public void testMultipleNumbers(){
-    	assertEquals(6, Calculator.add("1,2,3"));
-    }
+  public void testMultipleNumbers(){
+   	assertEquals(6, Calculator.add("1,2,3"));
+  }
   @Test
-  	public void testMultipleNumbers2() {
-  		assertEquals(256, Calculator.add("1,2,3,4,5,6,7,201,8,9,10"));
-  	}
+  public void testMultipleNumbers2() {
+  	assertEquals(256, Calculator.add("1,2,3,4,5,6,7,201,8,9,10"));
+  }
   @Test
-  	public void testNewlineSplitter() {
-  		assertEquals(6, Calculator.add("1\n2\n3"));
-  	}
+  public void testNewlineSplitter() {
+  	assertEquals(6, Calculator.add("1\n2\n3"));
+  }
   @Test
-  	public void testBothSplitters1() {
-  		assertEquals(6, Calculator.add("1\n2,3"));
-  	}
+  public void testBothSplitters1() {
+  	assertEquals(6, Calculator.add("1\n2,3"));
+ 	}
   @Test
-  	public void testBothSplitters2() {
-  		assertEquals(6, Calculator.add("1,2\n3"));
-  	}
-  	
+  public void testBothSplitters2() {
+  	assertEquals(6, Calculator.add("1,2\n3"));
+ 	} 	
   @Test
-  	public void testDifferentDelimiters1() {
-  		assertEquals(3, Calculator.add("//;\n1;2"));
-  	}
+  public void testDifferentDelimiters1() {
+  	assertEquals(3, Calculator.add("//;\n1;2"));
+  }
   @Test
-  	public void testDifferentDelimiters2() {
-  		assertEquals(6, Calculator.add("//%\n4%2"));
-  	}
-   @Test
-  	public void testCombinedDelimiters() {
-  		assertEquals(16, Calculator.add("//%\n4%2,5\n5"));
-  	}
+  public void testDifferentDelimiters2() {
+  	assertEquals(6, Calculator.add("//%\n4%2"));
+  }
+  @Test
+  public void testCombinedDelimiters1() {
+  	assertEquals(16, Calculator.add("//%\n4%2,5\n5"));
+  }
+  @Test
+  public void testCombinedDelimiters2() {
+  	assertEquals(80, Calculator.add("//;\n4;1\n30,45"));
+  }
 }
