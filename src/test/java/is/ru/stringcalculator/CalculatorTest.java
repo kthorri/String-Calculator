dcd01e20d582eaf9a00b406cbd80b65bb7202ead
Thorri <kristinn.thorri@gmail.com>
2015-10-15 10:55:08 +0000

    Added an extra test for multiple numbers

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -28,5 +28,9 @@ public class CalculatorTest {
     public void testMultipleNumbers(){
     	assertEquals(6, Calculator.add("1,2,3"));
     }
+  @Test
+  	public void testMultipleNumbers2() {
+  		assertEquals(256, Calculator.add("1,2,3,4,5,6,7,201,8,9,10"));
+  	}
 
 }
