817a63dfb689aa91e8cb6910bda28c45daf9f759
Thorri <kristinn.thorri@gmail.com>
2015-10-17 18:54:25 +0000

    added test for 2 different delimiters and function. Added splitnumbers that takes in delimiter and changed the add function

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -1,17 +1,28 @@
 package is.ru.stringcalculator;
 
-import java.util.*;
+import java.util.ArrayList;
+import  java.util.regex.Pattern;
 
 public class Calculator {
 
 	public static int add(String text){
 		String num = text;
+		//System.out.println(num);
+
 		if (num.startsWith("//[")) {
-			String deliminator = text.substring(text.indexOf("[") +1, text.lastIndexOf("]"));
-			num = num.replace("[", "");
-			num = num.replace("]", "");
-			num = num.replace(deliminator, "*");
-			num = convertDelimiters(num);
+			String delim = text.substring(text.indexOf("[") + 1, text.lastIndexOf("]"));
+			delim = delim.replace("[", "");
+			String[] delimarr = delim.split("]");
+			StringBuilder builder = new StringBuilder();
+			for(String s : delimarr) {
+			    builder.append(Pattern.quote(s));
+			    builder.append("|");
+			}
+			String delimiter = builder.toString();
+			delimiter = delimiter.substring(0, delimiter.length() - 1);
+			String numbers = text.substring(text.indexOf("\n") + 1, text.length());
+			return sum(splitNumbers(numbers, delimiter));
+
 
 		}
 		if (num.contains("//")) {
@@ -40,6 +51,9 @@ public class Calculator {
 
 	private static String[] splitNumbers(String numbers){
 		return numbers.split(",|\n");
+	}
+	private static String[] splitNumbers(String numbers, String delimiter){
+		return numbers.split(delimiter);
 	}
       
   private static int sum(String[] numbers){
--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -68,9 +68,15 @@ public class CalculatorTest {
   @Test
   public void testBiggerthan1000() {
     assertEquals(2, Calculator.add("1001,2"));
-  }
+  } 
   @Test
-  public void testDelimitersOfAnyLength(){
+  public void testLongerDelimiters(){
     assertEquals(6, Calculator.add("//[***]\n1***2***3"));
   }
+
+  @Test
+  public void testDifferentDelimitersOfLength(){
+    assertEquals(6, Calculator.add("//[*][%]\n1*2%3"));
+  }
+
 }
